0c5d04570fd5e05bff2d4cfb41f48eaadef08418
wwc953 <wwc953@163.com>
2025-10-12 10:19:07 +0800

    init

---
+++ src/main/java/org/example/springaiali/SpringaialiApplication.java
@@ -0,0 +1,13 @@
+package org.example.springaiali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaialiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaialiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springaiali/SpringaialiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springaiali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaialiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
