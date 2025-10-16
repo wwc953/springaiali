a6e98503c3d29e16b59ff6d9dd3fefe927d11b38
wangwc <wwc953@163.com>
2025-10-16 16:23:37 +0800

    redis向量存储

--- src/main/java/org/example/springaiali/controller/HelloController.java
+++ src/main/java/org/example/springaiali/controller/HelloController.java
@@ -2,17 +2,31 @@ package org.example.springaiali.controller;
 
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.Filter;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class HelloController {
     private static final String DEFAULT_PROMPT = "你是一个博学的智能聊天助手，请根据用户提问回答！";
@@ -64,6 +78,49 @@ public class HelloController {
         return this.chatClient.prompt(query)
                 .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .stream().content();
+    }
+
+    @Autowired
+    RedisVectorStore vectorStore;
+
+
+    @GetMapping("/import")
+    public void importData() {
+        log.info("start import data");
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id", "12345");
+        map.put("year", "2025");
+        map.put("name", "yingzi");
+
+        List<Document> documents = List.of(
+                new Document("世界很大，救赎就在眼前"),
+                new Document("你面向过去向前走，然后又转身面向未来。", Map.of("year", 2024)),
+                new Document("Spring AI 太棒了！！Spring AI 太棒了！！Spring AI 太棒了！！", map));
+        vectorStore.add(documents);
+    }
+
+    @GetMapping("/search/{str}")
+    public List<Document> search(@PathVariable String str) {
+        if (StringUtils.isBlank(str)) {
+            str = "Spring";
+        }
+        log.info("start search data: {}", str);
+        return vectorStore.similaritySearch(SearchRequest
+                .builder()
+                .query(str)
+                .topK(2)
+                .build());
+    }
+
+    @GetMapping("/deleteFilter")
+    public void deleteFilter() {
+        log.info("start delete data with filter");
+        FilterExpressionBuilder filterExpressionBuilder = new FilterExpressionBuilder();
+        Filter.Expression expression = filterExpressionBuilder.eq("name", "yingzi").build();
+
+        //TODO 删不掉?
+        vectorStore.delete(expression);
     }
 
 
