ea82fbc0d51ede524315902792bb73c7c72638ed
wangwc <wwc953@163.com>
2025-10-17 11:16:06 +0800

    pgv

--- src/main/java/org/example/springaiali/config/PgConfig.java
+++ src/main/java/org/example/springaiali/config/PgConfig.java
@@ -2,6 +2,7 @@ package org.example.springaiali.config;
 
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +12,16 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @ConditionalOnProperty(name = "vectorstore.type", havingValue = "pg")
 public class PgConfig {
 
+    @Value("${spring.ai.vectorstore.pgvector.initialize-schema}")
+    private Boolean initializeSchema;
+
     @Bean
     public PgVectorStore pgVectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
         return PgVectorStore.builder(jdbcTemplate, embeddingModel)
                 .dimensions(1536)                    // Optional: defaults to model dimensions or 1536
                 .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)       // Optional: defaults to COSINE_DISTANCE
                 .indexType(PgVectorStore.PgIndexType.HNSW)                     // Optional: defaults to HNSW
-                .initializeSchema(true)              // Optional: defaults to false
+                .initializeSchema(initializeSchema)              // Optional: defaults to false
                 .schemaName("public")                // Optional: defaults to "public"
                 .vectorTableName("vector_store")     // Optional: defaults to "vector_store"
                 .maxDocumentBatchSize(10000)         // Optional: defaults to 10000
