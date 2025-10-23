4a678ba1cec5a41d30ef04115cfb09407a8122e9
wangwc <wwc953@163.com>
2025-10-23 16:25:43 +0800

    更新

--- src/main/java/org/example/springaiali/config/ChatClientConfig.java
+++ src/main/java/org/example/springaiali/config/ChatClientConfig.java
@@ -6,6 +6,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
