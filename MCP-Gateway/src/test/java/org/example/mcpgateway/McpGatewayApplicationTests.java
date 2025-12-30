5bdbf810480203a55038606a0984fde7ca1fe4bd
wwc953 <wwc953@163.com>
2025-12-30 18:25:22 +0800

    存量http转mcp 网关

---
+++ MCP-Gateway/src/test/java/org/example/mcpgateway/McpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+//package org.example.mcpgateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class McpGatewayApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
