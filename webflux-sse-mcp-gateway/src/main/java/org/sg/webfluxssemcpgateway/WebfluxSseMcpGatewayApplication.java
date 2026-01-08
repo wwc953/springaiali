3ec4baa8db698de37d702f731689c19a645d78f3
wangwc <wwc953@163.com>
2026-01-08 11:27:30 +0800

    webflux-sse-mcp-gateway

---
+++ webflux-sse-mcp-gateway/src/main/java/org/sg/webfluxssemcpgateway/WebfluxSseMcpGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.sg.webfluxssemcpgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSseMcpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxSseMcpGatewayApplication.class, args);
+    }
+
+}
---
+++ webflux-sse-mcp-gateway/src/test/java/org/sg/webfluxssemcpgateway/WebfluxSseMcpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sg.webfluxssemcpgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSseMcpGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
